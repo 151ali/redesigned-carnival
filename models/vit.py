69309ee2e9cf078a87189f248c58d40cb84157e8
151ali <ali151yek@gmail.com>
2022-03-07 12:51:12 +0100

    🐛

--- models/vit.py
+++ models/vit.py
@@ -150,9 +150,9 @@ class ViT(nn.Sequential):
                 device="cpu",
                 **kwargs):
         super().__init__(
-            PoseEmbedding(num_poses, in_features, emb_size,device),
-            TransformerEncoder(depth, emb_size=emb_size,device = device, **kwargs),
-            ClassificationHead(emb_size, num_classes)
+            PoseEmbedding(num_poses, in_features, emb_size, device),
+            #TransformerEncoder(depth, emb_size=emb_size,device = device, **kwargs),
+            #ClassificationHead(emb_size, num_classes)
         )
 
 
