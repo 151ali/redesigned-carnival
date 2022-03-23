4f94c9d01bd2470b3734e088f0c10e37e9f2062a
151ali <ali151yek@gmail.com>
2022-03-23 15:14:49 +0100

    .

--- train.py
+++ train.py
@@ -66,9 +66,11 @@ model = generate_model(
 # print(model)
 
 # Distributed GPU training if CUDA can detect more than 1 GPU
+# Distributed GPU training if CUDA can detect more than 1 GPU
 if torch.cuda.device_count() > 1:
-    print(f"Using {torch.cuda.device_count()} GPUs")
-    model = nn.DataParallel(model, device_ids=cuda_devices)
+    devices_count = torch.cuda.device_count()
+    print(f"Using {devices_count} GPUs")
+    model = nn.DataParallel(model, device_ids=[i for i in range(devices_count)])
 
 print("Total Parameters:", sum([p.nelement() for p in model.parameters()]))
 
